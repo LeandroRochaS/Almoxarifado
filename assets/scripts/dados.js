5aca15599c61d0644fa9450416ccd49315edcbe5
Reginaldo Santana <regymatrix@gmail.com>
2023-12-11 12:21:51 -0300

    select preenchido via vetor

---
+++ assets/scripts/dados.js
@@ -0,0 +1,33 @@
+var categoriasDados = [
+    {
+      "codigo": 1,
+      "nomeCategoria": "Gestão"      
+    },
+    {
+        "codigo": 2,
+        "nomeCategoria": "Manutenção"
+    },
+    {
+        "codigo": 3,
+        "nomeCategoria": "Cliente"
+    }
+  ];
+
+  var motivoDados =[
+    {
+        "codigo": 1,
+        "motivo": "Planejamento",
+        "idCategoria": 1      
+    },
+    {
+        "codigo": 1,
+        "motivo": "Financeiro",
+        "idCategoria": 1      
+    },
+    {
+        "codigo": 1,
+        "motivo": "Máquina Queimou",
+        "idCategoria": 2      
+    }
+    
+  ]
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
 
 function inputAdicionarCorAoFocar(){
     var listaInputs = document.querySelectorAll('input[type="text"');
-    console.log(listaInputs.length)
+  
     listaInputs.forEach(function(campo){
         campo.addEventListener('focus', function(){
             campo.style.backgroundColor="lightblue";
@@ -15,4 +15,56 @@ function inputAdicionarCorAoFocar(){
     });
 }
 
+
+
+function carregarCategoriaMotivos(){
+    var selectCategoria = document.getElementById('categoriaMotivo');
+    selectCategoria.innerHTML="";
+    
+    var optionVazia = document.createElement("option");
+    optionVazia.value = -1;
+    optionVazia.text ="";
+    selectCategoria.add(optionVazia);
+
+    categoriasDados.forEach(function(categoria) {
+      
+        var option = document.createElement("option");    
+        option.value = categoria.codigo;
+        option.text = categoria.nomeCategoria;
+        selectCategoria.add(option);
+    });
+
+}
+
+function carregarMotivos(){
+    var selectCategoria = document.getElementById('Motivo');
+    selectCategoria.innerHTML="";
+
+    let idCat = document.getElementById('categoriaMotivo').value;
+    let motivosFiltrados = motivoDados.filter(function(item){
+        return item.idCategoria==idCat;
+    });
+
+    motivosFiltrados.forEach(function(itemMotivo) {
+      
+        var option = document.createElement("option");    
+        option.value = itemMotivo.codigo;
+        option.text = itemMotivo.motivo;
+        selectCategoria.add(option);
+    });
+}
+
+// document.getElementById("btnInserirItens").addEventListener('click',function(){
+
+// });
+
 inputAdicionarCorAoFocar();
+carregarCategoriaMotivos();
+document.getElementById('categoriaMotivo').addEventListener('change',function(){
+    carregarMotivos();
+});
+
+console.log(categoriasDados)
+console.log(motivoDados.filter(function(itens){
+    return itens.idCategoria==1
+}));
