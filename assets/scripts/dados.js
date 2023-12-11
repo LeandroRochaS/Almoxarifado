0dc1bf3b092c60efedac721e3a908c1929b27867
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-11 17:18:19 -0300

    feat: building project

---
+++ assets/scripts/dados.js
@@ -0,0 +1,47 @@
+const categorias = [
+  {
+    idCategoria: 1,
+    Descricao: "Gestão",
+  },
+  {
+    idCategoria: 2,
+    Descricao: "Cliente",
+  },
+  {
+    idCategoria: 3,
+    Descricao: "RP",
+  },
+];
+
+const motivos = [
+  {
+    idMotivo: 1,
+    Descricao: "Planejamento",
+    idCategoria: 1,
+  },
+  {
+    idMotivo: 2,
+    Descricao: "Financeiro",
+    idCategoria: 1,
+  },
+  {
+    idMotivo: 3,
+    Descricao: "Financeiro",
+    idCategoria: 2,
+  },
+];
+
+const produtos = [
+  {
+    idProduto: 1,
+    Descricao: "Papel A4",
+    Estoque: 10,
+    EstoqueMinimo: 5,
+  },
+  {
+    idProduto: 2,
+    Descricao: "Mel doçe",
+    Estoque: 5,
+    EstoqueMinimo: 5,
+  },
+];
---
+++ assets/scripts/main.js
@@ -0,0 +1,155 @@
+function validarFormulario() {
+  console.log("Validando formulário...");
+  event.preventDefault();
+
+  var idDepartamento = document.getElementById("idDepartamento");
+  var departamento = document.getElementById("departamento");
+  var dataRequisicao = document.getElementById("dataRequisicao");
+  var idFuncionario = document.getElementById("idFuncionario");
+  var NomeFuncionario = document.getElementById("NomeFuncionario");
+  var cargo = document.getElementById("cargo");
+  var categoriaMotivo = document.getElementById("categoriaMotivo");
+  var Motivo = document.getElementById("Motivo");
+  var prioridade = document.querySelectorAll(
+    'input[name="prioridade"]:checked'
+  );
+  var CodigoProduto = document.getElementById("CodigoProduto");
+  var DescricaoProduto = document.getElementById("DescricaoProduto");
+  var Estoque = document.getElementById("Estoque");
+  console.log(prioridade.value);
+
+  var inputs = [
+    idDepartamento,
+    departamento,
+    dataRequisicao,
+    idFuncionario,
+    NomeFuncionario,
+    cargo,
+    categoriaMotivo,
+    Motivo,
+    CodigoProduto,
+    DescricaoProduto,
+    Estoque,
+  ];
+
+  inputs.forEach(function (elemento) {
+    if (elemento.value == "" || elemento.value == null) {
+      exibirError(elemento.id);
+    } else {
+      ocultarError(elemento.id);
+    }
+  });
+
+  console.log("Validando campos obrigatórios...");
+
+  if (
+    typeof CodigoProduto.value != "number" ||
+    CodigoProduto.value < 0 ||
+    CodigoProduto.value == null
+  ) {
+    console.log("Código do produto inválido");
+    exibirError("CodigoProduto");
+
+    /*et errorProduto = document.getElementById("grupoProdutoError");
+
+    let pProduto = (document.createElement("p").innerText =
+      "Código do produto inválido");
+    errorProduto.appendChild(pProduto);
+    */
+  }
+
+  if (
+    typeof Estoque.value != "number" ||
+    Estoque.value < 0 ||
+    Estoque.value == null
+  ) {
+    console.log("Código do produto inválido");
+    exibirError("Estoque");
+    /*
+    let errorProduto = document.getElementById("grupoProdutoError");
+
+    let pProduto = (document.createElement("p").innerText =
+      "Código do produto inválido");
+    errorProduto.appendChild(pProduto);
+    */
+  }
+
+  console.log("Formulário validado com sucesso.");
+
+  return true;
+}
+
+function exibirError(idElemento) {
+  const elemento = document.getElementById(idElemento);
+  elemento.style.display = "block";
+  elemento.style.border = "1px solid red";
+}
+
+function ocultarError(idElemento) {
+  const elemento = document.getElementById(idElemento);
+  elemento.style.display = "none";
+  elemento.style.border = "1px solid #ccc";
+}
+
+function carregarCategorias() {
+  const selectCategoria = document.getElementById("categoriaMotivo");
+  selectCategoria.innerHTML = "";
+
+  const optionFirst = document.createElement("option");
+  optionFirst.value = -1;
+  optionFirst.text = "";
+  selectCategoria.appendChild(optionFirst);
+
+  categorias.forEach(function (categoria) {
+    let option = document.createElement("option");
+    option.value = categoria.idCategoria;
+    option.text = categoria.Descricao;
+    selectCategoria.add(option);
+  });
+}
+
+function carregarMotivos() {
+  const selectMotivo = document.getElementById("Motivo");
+  selectMotivo.innerHTML = "";
+
+  const optionFirst = document.createElement("option");
+  optionFirst.value = -1;
+  optionFirst.value = "";
+  selectMotivo.appendChild(optionFirst);
+
+  const valorCategoria = document.getElementById("categoriaMotivo");
+  const motivosFiltrados = motivos.filter(
+    (m) => m.idCategoria == valorCategoria
+  );
+  console.log(valorCategoria);
+  console.log(motivosFiltrados);
+
+  motivosFiltrados.forEach(function (motivo) {
+    let option = document.createElement("option");
+    option.value = motivo.idMotivo;
+    option.innerText = motivo.Descricao;
+    selectCategoria.add(option);
+  });
+}
+
+document.getElementById("CodigoProduto").addEventListener("blur", function () {
+  const idProduto = document.getElementById("CodigoProduto").value;
+  const produtoFiltrado = produtos.filter((p) => p.idProduto == idProduto);
+
+  if (produtoFiltrado.length == 0) {
+    document.getElementById("DescricaoProduto").value = "";
+    document.getElementById("Estoque").value = "";
+    return;
+  }
+  document.getElementById("DescricaoProduto").value =
+    produtoFiltrado[0].Descricao;
+  document.getElementById("Estoque").value = produtoFiltrado[0].Estoque;
+});
+
+document
+  .getElementById("categoriaMotivo")
+  .addEventListener("change", function () {
+    carregarMotivos();
+  });
+
+carregarCategorias();
