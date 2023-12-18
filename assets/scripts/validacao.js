a0f81534f426cf4e96f5be2416555085a5e07e0f
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-18 10:20:47 -0300

    fix: validated date input

--- assets/scripts/validacao.js
+++ assets/scripts/validacao.js
@@ -1,18 +1,18 @@
 function validarFormulario() {
   erros = 0;
-  console.log("Validando formulário...");
   event.preventDefault();
 
-  const inputs = document.querySelectorAll("input[name='validated']");
+  let inputs = document.querySelectorAll("input[name='validated']");
+  const inputDate = document.getElementById("dataRequesicao");
+
+  inputs = [...inputs, inputDate];
 
   // Valida os campos obrigatórios
   inputs.forEach((input) => {
     if (input.required || input.value == "" || input.value == null) {
-      console.log("Campo obrigatório não preenchido: " + input.id);
       exibirErro(input.id, "Campo obrigatório");
       erros++;
     } else {
-      console.log("Campo obrigatório preenchido: " + input.id);
       ocultarErro(input.id);
     }
   });
@@ -22,12 +22,9 @@ function validarFormulario() {
   selections.forEach((select) => {
     console.log(select.value);
     if (select.value == "" || select.value == 0 || select.value == -1) {
-      console.log("select obrigatória não preenchida: " + select.id);
       exibirErro(select.id, "");
       erros++;
     } else {
-      console.log("select obrigatória preenchida: " + select.id);
-
       ocultarErro(select.id);
     }
   });
