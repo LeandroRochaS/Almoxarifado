5bdeef235e79a994b3f63fadfec6ea9325a6c1f1
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-18 10:19:35 -0300

    feacture: add more data for tests

--- assets/scripts/data/dados.js
+++ assets/scripts/data/dados.js
@@ -39,6 +39,7 @@ const produtos = [
     Estoque: 10,
     Preco: 20,
     EstoqueMinimo: 5,
+    unidade: "Un",
   },
   {
     idProduto: 2,
@@ -46,6 +47,7 @@ const produtos = [
     Estoque: 1,
     Preco: 10,
     EstoqueMinimo: 15,
+    unidade: "Un",
   },
   {
     idProduto: 3,
@@ -53,6 +55,7 @@ const produtos = [
     Estoque: 10,
     Preco: 3,
     EstoqueMinimo: 15,
+    unidade: "Un",
   },
   {
     idProduto: 4,
@@ -60,6 +63,135 @@ const produtos = [
     Estoque: 50,
     Preco: 1,
     EstoqueMinimo: 30,
+    unidade: "Un",
+  },
+  {
+    idProduto: 5,
+    Descricao: "Lápis HB",
+    Estoque: 25,
+    Preco: 2,
+    EstoqueMinimo: 10,
+    unidade: "Un",
+  },
+  {
+    idProduto: 6,
+    Descricao: "Caderno 100 folhas",
+    Estoque: 15,
+    Preco: 15,
+    EstoqueMinimo: 5,
+    unidade: "Un",
+  },
+  {
+    idProduto: 7,
+    Descricao: "Régua 30cm",
+    Estoque: 30,
+    Preco: 1.5,
+    EstoqueMinimo: 10,
+    unidade: "Un",
+  },
+  {
+    idProduto: 8,
+    Descricao: "Caneta azul",
+    Estoque: 20,
+    Preco: 1.2,
+    EstoqueMinimo: 8,
+    unidade: "Un",
+  },
+  {
+    idProduto: 9,
+    Descricao: "Apontador",
+    Estoque: 8,
+    Preco: 0.5,
+    EstoqueMinimo: 4,
+    unidade: "Un",
+  },
+  {
+    idProduto: 10,
+    Descricao: "Borracha",
+    Estoque: 40,
+    Preco: 0.8,
+    EstoqueMinimo: 20,
+    unidade: "Un",
+  },
+  {
+    idProduto: 11,
+    Descricao: "Cola Bastão",
+    Estoque: 12,
+    Preco: 2.5,
+    EstoqueMinimo: 6,
+    unidade: "Un",
+  },
+  {
+    idProduto: 12,
+    Descricao: "Estojo",
+    Estoque: 18,
+    Preco: 5,
+    EstoqueMinimo: 8,
+    unidade: "Un",
+  },
+  {
+    idProduto: 13,
+    Descricao: "Tesoura",
+    Estoque: 7,
+    Preco: 3.5,
+    EstoqueMinimo: 3,
+    unidade: "Un",
+  },
+  {
+    idProduto: 14,
+    Descricao: "Pasta L de plástico",
+    Estoque: 22,
+    Preco: 4,
+    EstoqueMinimo: 12,
+    unidade: "Un",
+  },
+  {
+    idProduto: 15,
+    Descricao: "Grampeador",
+    Estoque: 5,
+    Preco: 8,
+    EstoqueMinimo: 2,
+    unidade: "Un",
+  },
+  {
+    idProduto: 16,
+    Descricao: "Cartolina A3",
+    Estoque: 30,
+    Preco: 1.5,
+    EstoqueMinimo: 15,
+    unidade: "Un",
+  },
+  {
+    idProduto: 17,
+    Descricao: "Caneta vermelha",
+    Estoque: 15,
+    Preco: 1.2,
+    EstoqueMinimo: 6,
+    unidade: "Un",
+  },
+  {
+    idProduto: 18,
+    Descricao: "Apagador",
+    Estoque: 10,
+    Preco: 2,
+    EstoqueMinimo: 4,
+    unidade: "Un",
+  },
+  {
+    idProduto: 19,
+    Descricao: "Folha sulfite",
+    Estoque: 100,
+    Preco: 0.5,
+    EstoqueMinimo: 50,
+    unidade: "Un",
+  },
+  {
+    idProduto: 20,
+    Descricao: "Clips metálicos",
+    Estoque: 50,
+    Preco: 0.3,
+    EstoqueMinimo: 20,
+    unidade: "Un",
   },
 ];
 
@@ -88,6 +220,72 @@ const funcionarios = [
     Cargo: "Auxiliar",
     idDepartamento: 3,
   },
+  {
+    idFuncionario: 5,
+    Nome: "Ana Oliveira",
+    Cargo: "Analista",
+    idDepartamento: 2,
+  },
+  {
+    idFuncionario: 6,
+    Nome: "Carlos Rocha",
+    Cargo: "Desenvolvedor",
+    idDepartamento: 2,
+  },
+  {
+    idFuncionario: 7,
+    Nome: "Amanda Lima",
+    Cargo: "Analista de Marketing",
+    idDepartamento: 4,
+  },
+  {
+    idFuncionario: 8,
+    Nome: "Lucas Fernandes",
+    Cargo: "Analista Financeiro",
+    idDepartamento: 4,
+  },
+  {
+    idFuncionario: 9,
+    Nome: "Fernanda Oliveira",
+    Cargo: "Designer",
+    idDepartamento: 5,
+  },
+  {
+    idFuncionario: 10,
+    Nome: "Pedro Mendes",
+    Cargo: "Engenheiro",
+    idDepartamento: 5,
+  },
+  {
+    idFuncionario: 11,
+    Nome: "Julia Costa",
+    Cargo: "Analista de Recursos Humanos",
+    idDepartamento: 6,
+  },
+  {
+    idFuncionario: 12,
+    Nome: "Rafaela Santos",
+    Cargo: "Estagiário",
+    idDepartamento: 6,
+  },
+  {
+    idFuncionario: 13,
+    Nome: "Miguel Oliveira",
+    Cargo: "Técnico de Suporte",
+    idDepartamento: 7,
+  },
+  {
+    idFuncionario: 14,
+    Nome: "Patrícia Souza",
+    Cargo: "Analista de Qualidade",
+    idDepartamento: 7,
+  },
+  {
+    idFuncionario: 15,
+    Nome: "Gustavo Silva",
+    Cargo: "Coordenador de Projetos",
+    idDepartamento: 8,
+  },
 ];
 
 const departamentos = [
@@ -103,7 +301,29 @@ const departamentos = [
     idDepartamento: 3,
     Descricao: "Financeiro",
   },
+  {
+    idDepartamento: 4,
+    Descricao: "Marketing",
+  },
+  {
+    idDepartamento: 5,
+    Descricao: "Design e Engenharia",
+  },
+  {
+    idDepartamento: 6,
+    Descricao: "Recursos Humanos",
+  },
+  {
+    idDepartamento: 7,
+    Descricao: "Suporte e Qualidade",
+  },
+  {
+    idDepartamento: 8,
+    Descricao: "Projetos",
+  },
 ];
+
+// Certifique-se de adicionar um ID único para cada novo departamento
 
 const requisicoes = [
   {
